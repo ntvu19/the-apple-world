e99a2a505f9c84fb1aa2e453d0f5af837d595cd8
Thanh Vu <63234743+thanhvvu34@users.noreply.github.com>
2021-06-19 00:12:36 +0700

    Read (CRUD)
    Read data from MongoDB use Mongoose

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,19 +1,19 @@
 const Course = require('../models/Course')
+const { multipleMongooseToObject } = require('../../util/mongoose')
 
 class SiteController {
 
     // [GET] /
-    index(req, res) {
+    index(req, res, next) {
 
-        Course.find({}, function(err, courses) {
-            if (!err) {
-                res.json(courses)
-                return
-            }
-            res.status(400).json({ error: 'ERROR' })
-        })
+        Course.find({})
+            .then(courses => {
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses)
+                })
+            })
+            .catch(next)
 
-        // res.render('home')
     }
 
 }
